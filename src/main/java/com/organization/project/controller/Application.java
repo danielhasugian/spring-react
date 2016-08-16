c75c7a976dc533e3b588fad57e6a50b534d1e9e0
alfian.subiyakto <673.alf@gmail.com>
2016-08-16 21:22:56 +0700

    get response from header and body json

--- src/main/java/com/organization/project/controller/Application.java
+++ src/main/java/com/organization/project/controller/Application.java
@@ -1,16 +1,17 @@
 package com.organization.project.controller;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class Application {
+	private static final int LISTENEDPORT = 88;
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+		System.getProperties().put("server.port", LISTENEDPORT);
+		SpringApplication.run(Application.class, args);
 		System.out.println("Let's inspect the beans provided by Spring Boot:");
 	}
 
---
+++ src/main/java/com/organization/project/controller/GreetingController.java
@@ -0,0 +1,39 @@
+package com.organization.project.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.organization.project.pojo.Greeting;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	private HashMap<String, Object> mapOut;
+
+	@RequestMapping(method = RequestMethod.GET, value = "/coba")
+	public HashMap<String, Object> greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
+		mapOut = new HashMap<>();
+		Greeting greeting = new Greeting(counter.incrementAndGet(), String.format(template, name));
+		mapOut.put("failed", false);
+		mapOut.put("result", greeting);
+		return mapOut;
+	}
+
+	@RequestMapping(method = RequestMethod.PUT)
+	public HashMap<?, ?> halo(@RequestBody HashMap<?, ?> coba) {
+		mapOut = new HashMap<>();
+		mapOut.put("failed", false);
+		mapOut.put("result", coba);
+		return mapOut;
+	}
+}
