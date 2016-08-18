7439c203e67ad89389a08e371cb9f3ccf2c4307b
Daniel <danielhasugian.social@gmail.com>
2016-08-18 21:59:05 +0700

    Update

--- src/main/java/com/organization/project/server/ServletContainerCustomizer.java
+++ src/main/java/com/organization/project/server/ServletContainerCustomizer.java
@@ -5,14 +5,12 @@ import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomi
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
-import com.organization.project.util.PropertiesUtil;
-
 @PropertySource("classpath:config.properties")
 @Component
-public class ServletContainerCustomizer extends PropertiesUtil implements EmbeddedServletContainerCustomizer {
+public class ServletContainerCustomizer implements EmbeddedServletContainerCustomizer {
 	
 	@Override
 	public void customize(ConfigurableEmbeddedServletContainer container) {
-		container.setPort(Integer.valueOf(getPropertiesValue("organization.port")));
+		//container.setPort(Integer.valueOf(getPropertiesValue("organization.port")));
 	}
 }
